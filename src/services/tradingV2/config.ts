82b9bf0d1d473d6fff61acfa10cf1afb33beff0f
manojmarquez33 <manojmarquez33@gmail.com>
2026-03-01 08:05:19 +0530

    INITIAL_BASE_QUANTITY

--- src/services/tradingV2/config.ts
+++ src/services/tradingV2/config.ts
@@ -32,10 +32,10 @@ export class TradingConfig {
         CONFIRMATION_TIMEFRAME: "4h", // higher timeframe for confirmation
         STRUCTURE_TIMEFRAME: "1d", // higher timeframe for structure
         LEVERAGE: 20,
-        INITIAL_BASE_QUANTITY: 1,
+        INITIAL_BASE_QUANTITY: 7,
         TRADING_MODE: "aggressive",
         MIN_MOVEMENT_PERCENT: 0.6,
-        MAX_ALLOWED_PRICE_MOVEMENT_PERCENT: 3,
+        MAX_ALLOWED_PRICE_MOVEMENT_PERCENT: 4,
         MIN_ALLOWED_PRICE_MOVEMENT_PERCENT: 0.01,
         TAKE_PROFIT_PERCENT: 3000,
         SL_TRIGGER_BUFFER_PERCENT: 0.15,
